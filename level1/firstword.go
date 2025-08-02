81a7e58f80f658783135077cc723d8f002d975ce
danikeya danikeya <keyadaniel56@gmail.com>
2025-08-02 12:52:03 +0000

    firstword

---
+++ level1/firstword.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func trimFields(s string)string{
+	start:=0;
+	end:=len(s)-1
+
+	for start<=end &&s[start]==' '{
+		start++
+	}
+	for end>=start &&s[end]==' '{
+		end--
+	}
+	return s[start:end+1]
+}
+
+func firstword(s string)string{
+	word:=trimFields(s)
+	start:=0
+	for start>=0 && word[start]!=' '{
+    start++
+	}
+	return word[:start]
+}
+
+func main(){
+	result:="hello world"
+	fmt.Println(firstword(result))
+}
