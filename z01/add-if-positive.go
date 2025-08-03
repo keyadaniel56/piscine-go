7a61585647c8b302e29a6b31a08936de2988e5e9
danikeya danikeya <keyadaniel56@gmail.com>
2025-08-03 14:25:09 +0000

    add if positive

---
+++ z01/add-if-positive.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func AddIfPositive(a int, b int) int {
+	// your code here
+	if a >= 0 && b >= 0 {
+		return a + b
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(AddIfPositive(1, 2))
+	fmt.Println(AddIfPositive(1, -2))
+	fmt.Println(AddIfPositive(-1, 2))
+	fmt.Println(AddIfPositive(-1, -2))
+	fmt.Println(AddIfPositive(10, 20))
+	fmt.Println(AddIfPositive(0, 20))
+}
