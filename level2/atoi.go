887757975a7960fa121e408a3d2278e5373309a8
danikeya danikeya <keyadaniel56@gmail.com>
2025-08-03 08:57:04 +0000

    atoi

---
+++ level2/atoi.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+
+func Atoi(s string)int{
+	start:=0
+	sign:=1
+	result:=0
+	if s[0]=='-'{
+		sign=-1
+		start=1
+	}
+	if s[0]=='+'{
+		sign=1
+	}
+	for i:=start;i<len(s);i++{
+		if s[i]<0 && s[i]>9{
+			return 0
+		}
+		digit:=int(s[i]-'0')
+		result=result*10+digit
+	}
+	return result * sign
+}
+
+func main(){
+	fmt.Println(Atoi("-123"))
+}
