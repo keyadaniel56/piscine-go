b1090f4036c580658a5dfbb8157f73384a31cbb6
danikeya danikeya <keyadaniel56@gmail.com>
2025-08-04 13:32:54 +0000

    active-bits

--- slice-manipulation/active-bits.go
+++ slice-manipulation/active-bits.go
@@ -16,3 +16,31 @@ func CountActiveBits(n int) int {
 func main(){
 	fmt.Println(CountActiveBits(3))
 }
+
+
+
+// Instructions
+// Write a function, ActiveBits, that returns the number of active bits (bits with the value 1) in the binary representation of an integer number.
+
+// Expected function
+// func ActiveBits(n int) int {
+
+// }
+// Usage
+// Here is a possible program to test your function :
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
+
+// func main() {
+// 	fmt.Println(piscine.ActiveBits(7))
+// }
+// And its output :
+
+// $ go run .
+// 3
+// $
