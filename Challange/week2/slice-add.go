1b8ccba01461277a812a94e4022f89dcff69c4c6
danikeya danikeya <keyadaniel56@gmail.com>
2025-08-06 13:26:22 +0000

    adding an element from a slice

---
+++ Challange/week2/slice-add.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func SliceAdd(slice []int , num int) []int {
+	return append(slice,num)
+}
+
+
+func main() {
+	fmt.Println(SliceAdd([]int{1, 2, 3}, 4))
+	fmt.Println(SliceAdd([]int{}, 4))
+}
