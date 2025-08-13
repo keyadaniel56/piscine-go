b482809ff8697d99b36e5a6f76e492018a211541
danikeya <keyadaniel56@gmail.com>
2025-08-13 22:52:06 +0300

    we are unique

---
+++ week4/wearunique.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func WeAreUnique(str1 , str2 string) int {
+	if len(str1)==0 && len(str2)==0{
+		return -1
+	}
+	set1:=make(map[rune]bool)
+	set2:=make(map[rune]bool)
+	
+	for _,char:=range str1{
+		set1[char]=true
+	}
+	for _,char:=range str2{
+		set2[char]=true
+	}
+
+	count:=0
+
+	for char:=range set1{
+		if !set2[char]{
+			count++
+		}
+	}
+	for char:=range set2{
+		if !set1[char]{
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(WeAreUnique("foo", "boo"))
+	fmt.Println(WeAreUnique("", ""))
+	fmt.Println(WeAreUnique("abc", "def"))
+}
