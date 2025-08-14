c47515bd6216568ac34788b82a573b7bffaa7a81
danikeya <keyadaniel56@gmail.com>
2025-08-14 14:40:52 +0300

    strlen

---
+++ week4/strlen.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func StrLen(s string) int {
+	count := 0
+	for range s {
+		count++
+	}
+	return count
+}
+
+func main() {
+	l := StrLen("Hello World!")
+	fmt.Println(l)
+}
