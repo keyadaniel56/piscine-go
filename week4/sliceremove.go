1867caaaf4fe0ee8b7b5ec2639ebf849d010432f
danikeya <keyadaniel56@gmail.com>
2025-08-14 15:49:21 +0300

    slice remove

---
+++ week4/sliceremove.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func SliceRemove(slice []int, num int) []int {
+
+	for i, v := range slice {
+		if v == num {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
+}
+
+func main() {
+	fmt.Println(SliceRemove([]int{1, 2, 3}, 2))
+	fmt.Println(SliceRemove([]int{4, 3}, 4))
+	fmt.Println(SliceRemove([]int{}, 1))
+
+}
