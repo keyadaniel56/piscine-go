aa79a7366ce2b4e29cbfaaee5ab4e821c885cea7
danikeya <keyadaniel56@gmail.com>
2025-08-15 14:00:07 +0300

    alphaposition

---
+++ week4/alphaposition.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func AlphaPosition(c rune) int {
+	// your code goes here
+	if c >= 'a' && c <= 'z' {
+		return int(c - 'a' + 1)
+	}
+	if c >= 'A' && c <= 'Z' {
+		return int(c - 'A' + 1)
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(AlphaPosition('a'))
+	fmt.Println(AlphaPosition('z'))
+	fmt.Println(AlphaPosition('B'))
+	fmt.Println(AlphaPosition('Z'))
+	fmt.Println(AlphaPosition('0'))
+	fmt.Println(AlphaPosition(' '))
+}
