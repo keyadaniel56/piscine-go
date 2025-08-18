cc3b51f9bfcbc3d1d1a85a0c04d52147023db0d7
danikeya <keyadaniel56@gmail.com>
2025-08-18 11:42:04 +0300

    countalpha

---
+++ checkpoint3_getting_ready/countalpha.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func CountAlpha(s string) int {
+	count:=0
+	for _,char:=range s{
+		if (char>='a' && char<='z') || (char>='A' && char<='Z'){
+			count++
+		}
+	}
+	return count
+}
+
+
+func main(){
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+}
