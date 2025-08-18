e828bcade68b04596f4ae8eda2aca55c54e1a6d4
danikeya <keyadaniel56@gmail.com>
2025-08-18 19:54:03 +0300

    cameltosnake

---
+++ checkpoint3_getting_ready/cameltosnakecase.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func isCamel(s string)bool{
+	if len(s)==0{
+		return false
+	}
+	for i:=0;i<len(s);i++{
+		char:=s[i]
+		if !(char>='a' && char<='z') && !(char>='A' && char<='Z'){
+			return false
+		}
+		if i>0 && s[i-1]>='A' && s[i-1]<'Z' && char>='A' && char<='Z'{
+			return false
+		}
+		if i==len(s)-1 && char>='A' && char<='Z'{
+			return false
+		}
+	}
+	return true
+}
+
+
+
+func camelToSnakeCase(s string)string{
+	if !isCamel(s){
+		return s
+	}
+	results:=""
+	for i:=0;i<len(s);i++{
+		char:=s[i]
+		if char>='A' && char<='Z'{
+			if i>0{
+				results+="_"
+			}
+			results+=string(char)
+		}else{
+			results+=string(char)
+		}
+	}
+	return results
+}
+
+
+
+func main() {
+	fmt.Println(camelToSnakeCase("HelloWorld"))
+	fmt.Println(camelToSnakeCase("helloWorld"))
+	fmt.Println(camelToSnakeCase("camelCase"))
+	fmt.Println(camelToSnakeCase("CAMELtoSnackCASE"))
+	fmt.Println(camelToSnakeCase("camelToSnakeCase"))
+	fmt.Println(camelToSnakeCase("hey2"))
+}
