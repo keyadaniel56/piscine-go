9336e000c2f187ca276edaa3871cd75148064d96
danikeya <keyadaniel56@gmail.com>
2025-08-18 21:16:58 +0300

    hashcode

---
+++ checkpoint3_getting_ready/hashcode.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func HashCode(dec string) string {
+	size := len(dec)
+	result := ""
+	for i := 0; i < size; i++ {
+		val := (int(dec[i]) + size)
+		if val < 33 {
+			val += 33
+		}
+		result += string(val)
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(HashCode("A"))           // B
+	fmt.Println(HashCode("AB"))          // CD
+	fmt.Println(HashCode("BAC"))         // EDF
+	fmt.Println(HashCode("Hello World")) // Spwwz+bz}wo
+}
