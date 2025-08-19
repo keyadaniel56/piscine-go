b06119d2cde0450b412df50069767edfbe0f8e4e
danikeya <keyadaniel56@gmail.com>
2025-08-19 07:59:01 +0300

    fromto

--- week4/fromto.go
+++ week4/fromto.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func Itoa(n int)string{
+func itoa(n int)string{
 	if n==0{
 		return "0"
 	}
@@ -14,39 +14,40 @@ func Itoa(n int)string{
 	var digits [] byte
 	for n>0{
 		d:=n%10
-		digits=append(digits, '0'+byte(d))
-		n/=10		
+		digits=append(digits, byte(d)+'0')
+		n/=10
 	}
 	for i,j:=0,len(digits)-1;i<j;i,j=i+1,j-1{
 		digits[i],digits[j]=digits[j],digits[i]
 	}
 	if isNegative{
-		return "-" + string(digits)
+		return "-"+string(digits)
 	}
 	return string(digits)
 }
 
-func FromTo(from,to int)string{
-	if from<0 || from>99 || to<0 || to>99{
-		return "Invalid"
+func FromTo(from int,to int)string{
+	if from<0 || from >99 || to<0 || to>99{
+		return "Invalid" +"\n"
 	}
-	step:=1
 	result:=""
+	step:=1
 	if from>to{
 		step=-1
 	}
 	for i:=from;;i+=step{
 		if i<10{
-			result+="0"
+			result +="0"
 		}
-		result+=Itoa(i)
+		result+=itoa(i)
 		if i==to{
 			break
 		}
 		result+=","
 	}
-	return result + "\n"
+return 	result+"\n"
 }
+
 
 func main() {
 	fmt.Print(FromTo(1, 10))
