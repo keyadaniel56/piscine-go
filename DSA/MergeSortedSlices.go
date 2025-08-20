f22c2b4488e0ebb86b8e3c6b107c1a72cfb2c176
danikeya <keyadaniel56@gmail.com>
2025-08-20 23:56:48 +0300

    mergesort

---
+++ DSA/MergeSortedSlices.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func MergeSortedSlices(a, b []int) []int {
+	result := []int{}
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			result = append(result, a[i])
+			i++
+		} else {
+			result = append(result, b[j])
+			j++
+		}
+	}
+	result = append(result, a[i:]...)
+	result = append(result, b[j:]...)
+	return result
+}
