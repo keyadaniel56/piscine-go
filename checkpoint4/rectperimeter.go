c5059b352923b6f48f3c6460490896f1e9ece91a
danikeya <keyadaniel56@gmail.com>
2025-08-21 16:27:13 +0300

    rectperimeter

---
+++ checkpoint4/rectperimeter.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func RectPerimeter(w, h int) int {
+	if w<1 || h<1{
+		return -1
+	}
+	return 2*(w+h)
+}
+
+func main() {
+	fmt.Println(RectPerimeter(10, 2))
+	fmt.Println(RectPerimeter(434343, 898989))
+	fmt.Println(RectPerimeter(10, -2))
+}
